09c43bdcd3e4d3d1d4950edf3a138e101a33a930
Pavel Vaitsikhouski <pavel.vaitsikhouski@adevinta.com>
2021-12-15 20:40:49 +0300

    Added percentage moving to remove handling

--- Skillmprove/Core/ViewModifiers/Cardify.swift
+++ Skillmprove/Core/ViewModifiers/Cardify.swift
@@ -9,39 +9,45 @@ import SwiftUI
 
 struct Cardify: ViewModifier {
     @State private var offset: CGSize = CGSize.zero
-    private var offsetToRemove: Double { 50 }
+    let thresholdPercentage: CGFloat = 0.25
     var onRemove: () -> Void
 
     func body(content: Content) -> some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 16)
-                .fill()
-                .foregroundColor(.white)
-            content
-        }
-        .aspectRatio(1.0, contentMode: .fit)
-        .shadow(radius: 5)
-        .rotationEffect(.degrees(Double(offset.width / 20)), anchor: .bottom)
-        .offset(x: offset.width, y: 0)
-        .gesture(
-            DragGesture()
-                .onChanged { gesture in
-                    offset = gesture.translation
-                }
-                .onEnded { _ in
-                    withAnimation {
-                        if abs(offset.width) > offsetToRemove {
-                            onRemove()
-                        } else {
-                            offset = .zero
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill()
+                    .foregroundColor(.white)
+                content
+            }
+            .aspectRatio(1.0, contentMode: .fit)
+            .shadow(radius: 5)
+            .rotationEffect(.degrees(Double(offset.width / 20)), anchor: .bottom)
+            .offset(x: offset.width, y: 0)
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        offset = gesture.translation
+                    }
+                    .onEnded { value in
+                        withAnimation {
+                            if abs(getGesturePercentage(geometry, from: value)) > thresholdPercentage {
+                                onRemove()
+                            } else {
+                                offset = .zero
+                            }
                         }
                     }
-                }
-        )
+            )
+        }
         .transition(
             .asymmetric(insertion: .identity,
                         removal: .move(edge: offset.width > 0 ? .trailing : .leading))
         )
+    }
+    
+    private func getGesturePercentage(_ geometry: GeometryProxy, from gesture: DragGesture.Value) -> CGFloat {
+        gesture.translation.width / geometry.size.width
     }
 }
 
