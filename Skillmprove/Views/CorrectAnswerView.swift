7757b2dc9700c48f19da1e7ff0ff33c1c9988a10
Pavel Vaitsikhouski <pavel.vaitsikhouski@adevinta.com>
2021-12-16 21:05:26 +0300

    Refactored code

--- Skillmprove/Views/CorrectAnswerView.swift
+++ Skillmprove/Views/CorrectAnswerView.swift
@@ -13,36 +13,49 @@ struct CorrectAnswerView: View {
     var body: some View {
         ZStack {
             Color.black.opacity(0.5)
-            VStack {
-                Spacer()
-                Text("🏆")
-                    .font(.system(size: 90))
-                Text("Congrats!")
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(.green)
-                Spacer()
-                Button {
-                    isPresented = false
-                } label: {
-                    Text("Next")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.white)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 64)
-                        .background(Color.red)
-                        .cornerRadius(16)
-                        .shadow(radius: 5)
-                }
-                .padding(.bottom, 16)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .aspectRatio(1.0, contentMode: .fit)
-            .background(Color.white)
-            .cornerRadius(16)
-            .padding(24)
-            .shadow(radius: 5)
+            card
         }
         .edgesIgnoringSafeArea(.all)
+    }
+    
+    var card: some View {
+        VStack {
+            Spacer()
+            cup
+            title
+            Spacer()
+            nextButton
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .aspectRatio(1.0, contentMode: .fit)
+        .background(Color.white)
+        .cornerRadius(16)
+        .padding(24)
+        .shadow(radius: 5)
+    }
+    var cup: some View {
+        Text("🏆")
+            .font(.system(size: 90))
+    }
+    var title: some View {
+        Text("Congrats!")
+            .font(.system(size: 24, weight: .bold))
+            .foregroundColor(.green)
+    }
+    var nextButton: some View {
+        Button {
+            isPresented = false
+        } label: {
+            Text("Next")
+                .font(.system(size: 18, weight: .semibold))
+                .foregroundColor(.white)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 64)
+                .background(Color.red)
+                .cornerRadius(16)
+                .shadow(radius: 5)
+        }
+        .padding(.bottom, 16)
     }
 }
 
