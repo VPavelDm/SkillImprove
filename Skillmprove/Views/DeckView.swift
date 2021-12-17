9f008b68bcee1b688b1e5d184fc23392988a7e72
Pavel Vaitsikhouski <pavel.vaitsikhouski@adevinta.com>
2021-12-17 18:22:25 +0300

    Added card dragging

--- Skillmprove/Views/DeckView.swift
+++ Skillmprove/Views/DeckView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct DeckView<Card, Item>: View where Card: View {
+    @State private var topCardOffset: CGSize = .zero
     var cards: [Item]
     var content: (Item) -> Card
         
@@ -18,13 +19,34 @@ struct DeckView<Card, Item>: View where Card: View {
                 content(cards[index])
                     .zIndex(Double(index))
                     .opacity(isCardVisible(at: index) ? 1 : 0)
+                    .offset(x: dragOffset(at: index).width, y: dragOffset(at: index).height)
                     .offset(x: 0, y: offset(at: index))
                     .scaleEffect(scale(at: index))
+                    .rotationEffect(dragRotation(at: index))
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                topCardOffset = value.translation
+                            }
+                            .onEnded { value in
+                                withAnimation {
+                                    topCardOffset = .zero
+                                }
+                            }
+                    )
             }
         }
     }
     
     // MARK: - UI Utils
+    private func dragRotation(at index: Int) -> Angle {
+        guard index == cards.count - 1 else { return .zero }
+        return .degrees(topCardOffset.width / 20)
+    }
+    private func dragOffset(at index: Int) -> CGSize {
+        guard index == cards.count - 1 else { return .zero }
+        return topCardOffset
+    }
     private func offset(at index: Int) -> Double {
         if isCardVisible(at: index) {
             return Double((cards.count - 1 - index) * -13)
